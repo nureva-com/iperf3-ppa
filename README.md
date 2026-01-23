181950d1bd860d55d62fe4ae03e41179e9c05bab
Kevin Lutzer <kevinlutzer@nureva.com>
2026-01-23 12:25:32 -0500

    iperf3 update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # iperf3-ppa
+PPA repository for libgpiod 2.X linux library. Note that this PPA uses the 2.X version of the libgpiod library which is still in active development. This package is not related at all to the [libgpiod2 debian package](https://packages.debian.org/sid/libgpiod2) which uses 1.6.X of the library.
+
+## Usage
+
+To use this PPA:
+
+``` bash
+curl -s --compressed "https://nureva-com.github.io/iperf3-ppa/KEY.gpg" | gpg --dearmor | sudo tee /usr/share/keyrings/nureva-iperf3-archive-keyring.gpg
+sudo curl -s --compressed -o /etc/apt/sources.list.d/nureva-iperf3.list "https://nureva-com.github.io/iperf3-ppa/nureva-iperf3.list"
+sudo apt update
+sudo apt-get install -y iperf3_3.2
+```
+
+The supported architectures are arm64, and amd64.
+
+## Building the Packages
+
+Run `./bin/build_deb.sh`. Copy the resulting .deb files under `$PWD/.wrk` to this directory.
+
+## Updating/adding new Packages to the PPA
+
+Put your new .deb files inside the git repo and run `./bin/generate.sh` to
+update the files. Then commit and push to repo.
---
+++ bin/build_deb.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+## This script is meant to setup ad debian package for iperf3 version 3.2 which at this time does not have it's
+## own ppa. 
+
+IPERF3_GIT_VERSION=3.2-STABLE
+IPERF3_SEMANTIC_VERSION=3.2
+
+ARCH=$(uname -m)
+
+# Check that all needed packages are installed
+INSTALL_COUNT=$(dpkg-query -l build-essential libtool pkg-config automake autoconf pkg-config libssl-dev | grep "ii" -c)
+if  [ $INSTALL_COUNT -ne 6 ]; then
+    echo "Please run 'sudo apt install -y build-essential libtool pkg-config automake autoconf pkg-config libssl-dev' to install dependencies"
+    exit 1
+fi
+
+if [ "$ARCH" == "aarch64" ]; then
+    export ARCH_SUFFIX=arm64
+elif [ "$ARCH" == "x86_64" ]; then
+    export ARCH_SUFFIX=amd64
+else 
+    echo "Architecture $ARCH not supported here"
+    exit 1
+fi
+
+echo "Building iperf3"
+
+WRK_DIR=$PWD/.wrk
+PACKAGE_DIR=$WRK_DIR/package
+mkdir -p $PACKAGE_DIR
+
+# We clone iperf3 repo
+if [[ ! -d $WRK_DIR/iperf3 ]]; then
+    git clone \
+        --recursive https://github.com/esnet/iperf.git \
+        -b "${IPERF3_GIT_VERSION}" \
+        $WRK_DIR/iperf3
+
+    if [ $? -ne 0 ]; then
+        echo "failed to clone repo quiting."
+        exit 1
+    fi
+fi
+
+# Build iperf3 
+echo "Build iperf3..."
+(cd $WRK_DIR/iperf3 && ./bootstrap.sh)
+(cd $WRK_DIR/iperf3 && ./configure)
+(cd $WRK_DIR/iperf3 && make)
+
+# Build package directories
+mkdir -p $PACKAGE_DIR/DEBIAN
+mkdir -p $PACKAGE_DIR/usr/bin
+
+# Add control text to bin package
+cat << EOF > $PACKAGE_DIR/DEBIAN/control 
+Package: iperf3_${IPERF3_SEMANTIC_VERSION}
+Version: ${IPERF3_SEMANTIC_VERSION}
+Section: universe/libs
+Priority: optional
+Source: iperf3_${IPERF3_SEMANTIC_VERSION}
+Architecture: $ARCH_SUFFIX
+Maintainer: Kevin Lutzer <kevinlutzer@nureva.com>
+Description: iperf3 ${IPERF3_SEMANTIC_VERSION} for Ubuntu Linux, made for Ubuntu 24.04
+Homepage: https://github.com/esnet/iperf
+EOF
+
+# Copy all of the files into the package directories headers go into the package
+cp -r $WRK_DIR/iperf3/src/iperf3 $PACKAGE_DIR/usr/bin
+
+# Build the packages
+dpkg-deb --root-owner-group --build $PACKAGE_DIR $WRK_DIR/iperf3_${IPERF3_SEMANTIC_VERSION}_${ARCH_SUFFIX}.deb
